746a098bc0230a2c757bc54ef587af9bba100723
Vladimir Kolesnikov <9858713@gmail.com>
2026-03-16 02:45:40 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tech Stack
+
+- Node.js (v24.14.0)
+- React (v19.2.4)
+- TypeScript (v5.9.3)
+- Vite (v8.0.0)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div id="center">
+        <h1>Pet Store</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
